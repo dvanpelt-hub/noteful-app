e1af6b5e0290497903c6fce9450a732bca659cb4
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-02-03 12:59:01 +0000

    Updated vercel

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://git.heroku.com/protected-gorge-89954.git'
+  API_ENDPOINT: 'postgres://kxycqnfjgbxmjg:76271f130578ce4931635c36c0066834bd6faaf289d36503e6cfcf673995ea8a@ec2-54-234-28-165.compute-1.amazonaws.com:5432/d1ls01fk5oahs9'
   // API_ENDPOINT: 'http://localhost:8000'
 }
