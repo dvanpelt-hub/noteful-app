104cb8ff26615f39faf9dc11983110b4bbea8e19
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-02-03 13:08:59 +0000

    Updated API URL

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'postgres://kxycqnfjgbxmjg:76271f130578ce4931635c36c0066834bd6faaf289d36503e6cfcf673995ea8a@ec2-54-234-28-165.compute-1.amazonaws.com:5432/d1ls01fk5oahs9'
+  API_ENDPOINT: 'https://protected-gorge-89954.herokuapp.com/'
   // API_ENDPOINT: 'http://localhost:8000'
 }
