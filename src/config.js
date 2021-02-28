ab7019d7ea5710516834b47c03f841a6f21611bc
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-02-28 13:10:35 +0000

    Updated express

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://protected-gorge-89954.herokuapp.com/'
+  API_ENDPOINT: "https://protected-gorge-89954.herokuapp.com/api",
   // API_ENDPOINT: 'http://localhost:8000'
-}
+};
