902ba09588905dad4b783b547fb652898852b8c6
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-03-02 13:32:44 +0000

    Update

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -31,10 +31,8 @@ export default class Note extends React.Component {
         this.context.deleteNote(noteId);
         // allow parent to perform extra behaviour
         this.props.onDeleteNote(noteId);
-        // this.props.history.push("/");
-      })
-      .then(() => {
         this.forceUpdate();
+        // this.props.history.push("/");
       })
       .catch((error) => {
         console.error({ error });
