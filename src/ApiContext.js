02212543b9d1d216e79200cf853333d76d57e625
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-03-02 12:53:41 +0000

    Updated console.log

--- src/ApiContext.js
+++ src/ApiContext.js
@@ -1,9 +1,12 @@
-
-import React from 'react'
+import React from "react";
 export default React.createContext({
   notes: [],
   folders: [],
-  addFolder: (responseJson) => {console.log(responseJson)},
-  addNote: (responseJson) => {console.log(responseJson)},
+  addFolder: (responseJson) => {
+    console.log(responseJson);
+  },
+  addNote: (responseJson) => {
+    console.log(responseJson);
+  },
   deleteNote: () => {},
-})
+});
